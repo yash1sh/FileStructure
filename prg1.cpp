4765b26e3a1c1ab3c50ed404d99420edad86155d
Yash Shetty <yashshetty@gmail.com>
2022-05-26 09:40:00 +0530

    Addedd the pst grp

---
+++ prg1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main()
+{
+    fstream f1;
+    char ch;
+    char input[20];
+    cout<<"Enter the file name\n";
+    cin>>input;
+    f1.open(input,ios::in);
+    f1.unsetf(ios::skipws);
+    while(1)
+    {
+        f1>>ch;
+        if(f1.fail()) break;
+        cout<<ch;
+    }
+    return 0;
+
+}
